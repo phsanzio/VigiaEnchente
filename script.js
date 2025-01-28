df1b111b19be70d04421669869b5fa8bd88ca897
Pedro Sanzio <72276805+phsanzio@users.noreply.github.com>
2025-01-28 17:17:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const container = document.getElementById('box');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const box = document.querySelector('.box');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+  
+    registerBtn.addEventListener('click', () => {
+        box.classList.add("active");
+    });
+  
+    loginBtn.addEventListener('click', () => {
+        box.classList.remove("active");
+    });
+  });
